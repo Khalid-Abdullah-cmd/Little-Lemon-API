3693a521a29e39670936c5aa0b42de776fa910e3
Khalid-Abdullah-cmd <khalidabdullahmohammed4@gmail.com>
2026-06-14 15:31:50 +0300

    initial commit

---
+++ LittleLemonAPI/models.py
@@ -0,0 +1,76 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    
+    slug = models.SlugField(unique=True)
+    title = models.CharField(max_length=250)
+    
+    def __str__(self):
+        return self.title
+    
+    
+class MenuItem(models.Model):
+    
+    title = models.CharField(max_length=250)    
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    featured = models.BooleanField(default=False)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    
+    def __str__(self):
+        return self.title
+    
+
+
+class Cart(models.Model):
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    menu_items = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    unitprice = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    
+    def __str__(self):
+        return str(self.id)
+    
+    #  Prevents duplicate rows for the same user/item combo
+    class Meta:
+        unique_together = ('user', 'menu_items')
+    
+    
+class Order(models.Model):
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    delivery_crew = models.ForeignKey(User,
+    on_delete=models.SET_NULL,
+    null=True,
+    related_name='deliveries',
+    blank=True)
+    
+    status = models.BooleanField(default=False)
+    total = models.DecimalField(max_digits=6, decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return str(self.id)
+    
+    
+class OrderItem(models.Model):
+    
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    menuitem = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    
+    def __str__(self):
+        return str(self.id)
+    
+    
+            
+    
+    
+          
+    
